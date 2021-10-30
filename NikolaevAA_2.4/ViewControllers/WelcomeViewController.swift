3bd236e191ee9f0a6cdf242cc95286e92d77ceb2
AANikolaev <mjop41@gmail.com>
2021-10-30 16:44:57 +0300

    welcome viewcontroler

---
+++ NikolaevAA_2.4/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  NikolaevAA_2.4
+//
+//  Created by Anton Nikolaev on 30.10.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
